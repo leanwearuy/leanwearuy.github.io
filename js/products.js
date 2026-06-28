068ee0b3bde17002326a150b5384ea7eacd09404
leanwearuy <andresgass2840@gmail.com>
2026-06-28 16:42:35 -0300

    editar: Jordan 4 Thunder

--- js/products.js
+++ js/products.js
@@ -29,9 +29,9 @@ const PRODUCTOS = [
     precio: 0,
     talles: ["40 40.5 41 42 42.5 43 44 44.5 45 46 47.5"],
     imagenes: [
-      "assets/jordan/jordan-4-thunder-1.jpeg",
-      "assets/jordan/jordan-4-thunder-1.jpeg",
+      "assets/jordan/jordan-4-thunder-5.jpeg",
       "assets/jordan/jordan-4-thunder-2.jpeg",
+      "assets/jordan/jordan-4-thunder-1.jpeg",
       "assets/jordan/jordan-4-thunder-3.jpeg",
       "assets/jordan/jordan-4-thunder-4.jpeg",
     ],
