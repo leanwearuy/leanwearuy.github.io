f91c7de64b9eaad6cf0ba91edb899b4fb8f9e1cf
leanwearuy <andresgass2840@gmail.com>
2026-06-28 21:26:05 -0300

    editar: Jordan 4 Toro Bravo

--- js/products.js
+++ js/products.js
@@ -30,6 +30,10 @@ const PRODUCTOS = [
     talles: ["36","36.5","37.5","38","38.5","39","40","40.5","41","42","42.5","43","44","44.5","45","46","47.5"],
     imagenes: [
       "assets/jordan/jordan-4-toro-bravo-1.jpeg",
+      "assets/jordan/jordan-4-toro-bravo-2.jpeg",
+      "assets/jordan/jordan-4-toro-bravo-3.jpeg",
+      "assets/jordan/jordan-4-toro-bravo-4.jpeg",
+      "assets/jordan/jordan-4-toro-bravo-5.jpeg",
     ],
   },
   {
