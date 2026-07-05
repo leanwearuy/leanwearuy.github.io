447488788e9e219804c73080dbf28617c9dd2d07
leanwearuy <andresgass2840@gmail.com>
2026-07-05 18:37:36 -0300

    editar: Jordan 5 Midnight Navy

--- js/products.js
+++ js/products.js
@@ -303,6 +303,7 @@ const PRODUCTOS = [
     marca: "Jordan", categoria: "zapatos", seccion: "encargues",
     precio: 0,
     talles: ["36","36.5","37.5","38","39","40","40.5","41","42","42.5","43","44","44.5","45","46","47.5"],
+    colores: ["8990"],
     imagenes: [
       "assets/jordan/jordan-5-midnight-navy-1.jpeg",
       "assets/jordan/jordan-5-midnight-navy-2.jpeg",
