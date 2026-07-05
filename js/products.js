ab9946ede14e9fc6cb1d844686f7e9114e5f5ca6
leanwearuy <andresgass2840@gmail.com>
2026-07-05 18:28:18 -0300

    editar: Jordan 4 Infrared

--- js/products.js
+++ js/products.js
@@ -237,8 +237,8 @@ const PRODUCTOS = [
   {
     nombre: "Jordan 4 Infrared",
     marca: "Jordan", categoria: "zapatos", seccion: "encargues",
-    precio: 0,
-    talles: ["36","36.5","37.5","38","38.5","39","40","40.5","41","42","42.5","43","44","44.5","45","46","47.5"],
+    precio: 6890,
+    talles: ["40","40.5","41","42","42.5","43","44","44.5","45","46","47.5"],
     imagenes: [
       "assets/jordan/jordan-4-infrared-1.jpeg",
       "assets/jordan/jordan-4-infrared-2.jpeg",
